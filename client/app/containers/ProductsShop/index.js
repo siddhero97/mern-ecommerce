c942250e1570008c62855afc362ed99afdfd8147
Siddhartha Gupta <siddhero97@gmail.com>
2020-11-15 23:16:35 +0530

    Changed index stuff for server hosting

--- client/app/containers/ProductsShop/index.js
+++ client/app/containers/ProductsShop/index.js
@@ -18,44 +18,54 @@ class ProductsShop extends React.PureComponent {
     const slug = this.props.match.params.slug;
     this.props.fetchProducts(slug);
   }
+  f1(){
+    let min=0;
+    let max=1000;
+    console.log("inside f1: min ", min, " max ", max)
+  }
+  f2(){
+    let min=0;
+    let max=25;
+    console.log("inside f2: min ", min, " max ", max)
+    this.products = this.products.filter((product) => {
+      return product.price < 25;
+    })
+  }
+  f3(){
+    let min=25;
+    let max=50;
+    console.log(min);
+    console.log(max);
+    console.log("inside f3: min ", min, " max ", max)
+    this.products = this.products.filter((product) => {
+      return (product.price >= 25) && (product.price <50);
+    })
+
+  }
+  f4(){
+    let min=50;
+    let max=1000;
+    console.log("inside f4: min ", min, " max ", max)
+  }
   render() {
-    const { products, isLoading } = this.props;
+     const { products, isLoading } = this.props;
+    this.products = this.props.products;
+    this.isLoading = this.props.isLoading;
     let min;
     let max;
     min=0;
     max=1000;
     
-    let f1 = () =>{
-      min=0;
-      max=1000;
-    }
-    let f2 = () =>{
-      min=0;
-      max=25;
-    }
-    let f3 = () =>{
-      min=25;
-      max=50;
-      console.log(min);
-      console.log(max);
-
-    }
-    console.log(min);
     
-    console.log(max);
-    let f4 = () =>{
-      min=50;
-      max=1000;
-    }
     return (
 
       <div className='products-shop'>
         <div className="filters">
         <h1>price</h1>
-        <a href="#" onClick={f1}>any price</a>
-        <a href="#" onClick={f2}>under CA$25</a>
-        <a href="#" onClick={f3}>CA$25 to CA$50</a>
-        <a href="#" onClick={f4}>over CA$50</a>
+        <a href="#" onClick={this.f1}>any price</a>
+        <a href="#" onClick={this.f2}>under CA$25</a>
+        <a href="#" onClick={this.f3}>CA$25 to CA$50</a>
+        <a href="#" onClick={this.f4}>over CA$50</a>
         <br/>
         <h1>type of food</h1>
         <a href="#">main course</a>
@@ -66,8 +76,8 @@ class ProductsShop extends React.PureComponent {
         <div className='product-container'>
         {isLoading ? (
           <LoadingIndicator />
-        ) : products.length > 0 ? (
-          <ProductList products={products} min={min} max={max} />
+        ) : this.products.length > 0 ? (
+          <ProductList products={this.products} min={min} max={max} />
          
         ) : (
           <NotFound message='no products found.' />
